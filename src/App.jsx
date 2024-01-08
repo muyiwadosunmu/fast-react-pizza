4e2caafe9029bce9d993d842133644ecbb9bda24
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-08 17:24:40 +0100

    Dealing with Errors in our App

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,21 @@ import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 // For us to use powerful features like data loaders, data fetchers and data actions we needed to create a new router using this snippet below
 const router = createBrowserRouter([
   {
     element: <AppLayout />, //This is technically called a layout route because it doesn't have a path
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
