93a21ea340d52c52ffd5ad9f53db934265a12820
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-08 22:52:29 +0100

    Implemented Error Handling in Form actions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -32,7 +32,11 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
-  const [withPriority, setWithPriority] = useState(false);
+  // const [withPriority, setWithPriority] = useState(false);
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
   const cart = fakeCart;
   // console.log(cart);
 
@@ -53,6 +57,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -75,7 +80,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing Order" : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
@@ -92,8 +99,14 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
-  console.log(order);
 
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please give us your correct phone number we might need it to contact you!!";
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is OKAY create new order and redirect
   const newOrder = await createOrder(order);
   return redirect(`/order/${newOrder.id}`);
 }
