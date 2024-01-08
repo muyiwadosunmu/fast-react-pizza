3c2ba00759240a200870dfd6147cd2444fe99a69
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-08 16:31:36 +0100

    Building the App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
 
 // For us to use powerful features like data loaders, data fetchers and data actions we needed to create a new router using this snippet below
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "/order/:orderId", element: <Order /> },
+  {
+    element: <AppLayout />, //This is technically called a layout route because it doesn't have a path
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
 ]);
 
 function App() {
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React co</Link>
+      <p>Muyiwa</p>
+    </header>
+  );
+}
+
+export default Header;
