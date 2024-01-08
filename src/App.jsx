a3d25c817317f174ee2a969e6dd2666fba1c5d5e
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-08 16:01:39 +0100

    A newer way of implementing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+
+// For us to use powerful features like data loaders, data fetchers and data actions we needed to create a new router using this snippet below
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+]);
+
 function App() {
-  return <div>Hello World</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
