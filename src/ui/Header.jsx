bc7ea9a1ba711e8a906b37d551e28ffcbeb94080
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-09 12:19:11 +0100

    Styling Texts

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/users/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React co</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza co.
+      </Link>
       <SearchOrder />
-      <p>Muyiwa</p>
+      <Username />
     </header>
   );
 }
