369af3476f0da8c0312993e693be466650c4f24f
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-09 12:49:49 +0100

    Box Model Spacing

--- src/features/users/Username.jsx
+++ src/features/users/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Muyiwa</div>;
+  return <div className="hidden text-sm font-semibold">Muyiwa</div>;
 }
 
 export default Username;
