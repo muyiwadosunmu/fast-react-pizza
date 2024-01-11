8b918695bdef327897f0f3161431c849d3bd62e4
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-11 15:23:47 +0100

    Configuring Tailwind Custom Font

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
-import SearchOrder from '../features/order/SearchOrder';
-import Username from '../features/users/Username';
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/users/Username";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b border-stone-300 bg-yellow-500 px-4 py-3  uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza co.
       </Link>
