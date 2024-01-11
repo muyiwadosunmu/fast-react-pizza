e0c8422b94dfd17ffa105d17c10ec470137e9613
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-11 14:47:29 +0100

    Implemented absolute positioning on loader class

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,11 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import Header from './Header';
-import CartOverview from '../features/cart/CartOverview';
-import Loader from './Loader';
+import { Outlet, useNavigation } from "react-router-dom";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
