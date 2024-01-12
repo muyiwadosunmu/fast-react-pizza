82c7f4fbc6f97454b0b3daffec3ca1a1a520e525
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-12 12:06:05 +0100

    Reading and Updating the user state

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {useSelector} from 'react-redux'
 import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
@@ -33,6 +34,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username);
   // const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
@@ -50,7 +52,7 @@ function CreateOrder() {
       <Form method="POST" action="">
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input className="input grow" type="text" name="customer" defaultValue={username} required />
         </div>
 
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,5 +1,9 @@
-import CreateUser from '../features/users/CreateUser';
+import { useSelector } from "react-redux";
+import CreateUser from "../features/users/CreateUser";
+import Button from "./Button";
+
 function Home() {
+  const username = useSelector((state) => state.user.username);
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8  text-xl font-semibold text-stone-700 md:text-3xl ">
@@ -9,7 +13,11 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">Continue Ordering, {username}</Button>
+      )}
     </div>
   );
 }
